9cc616dc4d61f4f6c5a4b847f7ce5feafab588d5
imase0730@gmail.com <imase0730@gmail.com>
2025-04-19 02:10:18 +0900

    定数宣言。パドルの初期位置を設定。パドルが画面外へ移動しないように修正。

--- Main.cpp
+++ Main.cpp
@@ -7,18 +7,35 @@ int WINAPI WinMain(
 	_In_ int nCmdShow
 )
 {
-	// 変数の宣言
-	int key;
-	int x;
-	int y;
+	// ----- 定数宣言 ----- //
+	
+	// 画面のサイズ
+	const int SCREEN_WIDTH = 1280;
+	const int SCREEN_HEIGHT = 720;
 
-	// 変数の初期化
+	// パドルの大きさ
+	const int PADDLE_WIDTH = 32;
+	const int PADDLE_HEIGHT = 64;
+
+	// パドルの初期位置
+	const int PADDLE_START_X = 100;
+	const int PADDLE_START_Y = SCREEN_HEIGHT / 2 - PADDLE_HEIGHT / 2;
+
+	// パドルの移動の速さ
+	const int PADDLE_SPEED = 8;
+
+	// ----- 変数宣言 ----- //
+	int key;
+	int paddleX;
+	int paddleY;
+
+	// ----- 変数の初期化 ----- //
 	key = 0;
-	x = 0;
-	y = 0;
+	paddleX = PADDLE_START_X;
+	paddleY = PADDLE_START_Y;
 
 	// 画面モードのセット
-	SetGraphMode(1280, 720, 32);
+	SetGraphMode(SCREEN_WIDTH, SCREEN_HEIGHT, 32);
 
 	// ウインドウのタイトルを変更する
 	SetMainWindowText(L"Pong Game");
@@ -45,13 +62,23 @@ int WINAPI WinMain(
 		// 上を押していたら上に進む
 		if (key & PAD_INPUT_UP)
 		{
-			y = y - 10;
+			paddleY = paddleY - PADDLE_SPEED;
 		}
 
 		// 下を押していたら下に進む
 		if (key & PAD_INPUT_DOWN)
 		{
-			y = y + 10;
+			paddleY = paddleY + PADDLE_SPEED;
+		}
+
+		// パドルが画面外に出ないよに位置を補正する
+		if (paddleY < 0)
+		{
+			paddleY = 0;
+		}
+		if (paddleY > SCREEN_HEIGHT - PADDLE_HEIGHT)
+		{
+			paddleY = SCREEN_HEIGHT - PADDLE_HEIGHT;
 		}
 
 		// 画面を初期化する
@@ -60,7 +87,7 @@ int WINAPI WinMain(
 		// ----- ゲームの描画処理 ----- //
 
 		// パドルの描画
-		DrawBox(x, y, x + 32, y + 64, GetColor(255, 255, 255), TRUE);
+		DrawBox(paddleX, paddleY, paddleX + PADDLE_WIDTH, paddleY + PADDLE_HEIGHT, GetColor(255, 255, 255), TRUE);
 
 		// 裏画面の内容を表画面に反映させる
 		ScreenFlip();
